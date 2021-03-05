6ccad2311445be3d409f85489ff9395fc9442564
Oscar Djupfeldt <oscar@djupfeldt.se>
2021-03-05 08:49:29 +0100

    Routing

--- src/crews/Crew.svelte
+++ src/crews/Crew.svelte
@@ -6,19 +6,21 @@
   import Equipment from '../equipment/Equipment.svelte'
   import WeaponRows from '../weapons/WeaponRows.svelte'
   import Photo from '../Photo.svelte'
-  import PrintView from './PrintView.svelte'
 
 	import {store} from '../store.js';
 	import {crewBuilder} from '../crewBuilder.js';
 	import {crewValidator} from '../crewValidator.js';
+  import {location} from 'svelte-spa-router'
+  import {push, pop, replace} from 'svelte-spa-router'
 
   import recruits from '../data/recruits.json'
 
-	export let crew = {}
-	export let show
-	export let edit = false
+  export let params = {}
+
+	let crew = params.crew === 'new' ? crewBuilder.create("",0) : store.crews.find(c => c.id === params.crew)
+	let show
+  let edit = $location.endsWith('edit')
 	let compact = true
-  let print = false
 
 	crew = crewValidator.validateModel(crew)
 
@@ -27,89 +29,86 @@
 	let selectedRecruit;
 
 	function saveCrew() {
-	  var index = store.crews.findIndex(c => c.id === crew.id)
+	  let index = store.crews.findIndex(c => c.id === crew.id)
 	  if (index !== -1) {
 	    store.crews.splice(index, 1, crew)
 	  } else {
 	    store.crews.push(crew)
 	  }
 	  store.save(store.crews)
+    replace('/crew/' + crew.id + '/edit')
 	}
 </script>
 <div class="no-print">
-  <button on:click={() => edit = !edit}>{#if edit}Lock{:else}Edit{/if}</button>
+  <button on:click={() => replace('/crew/' + crew.id + '/' + (edit ? 'view' : 'edit'))}>{#if edit}Lock{:else}Edit{/if}</button>
   <button on:click={() => compact = !compact}>{#if compact}Full{:else}Compact{/if}</button>
-  <button on:click={() => print = !print}>{#if print}Regular View{:else}Print View{/if}</button>
-  <button on:click={() => show = false}>Back</button>
+  <button on:click={() => push('/print/' + crew.id)}>Print View</button>
 </div>
-{#if print}
-  <PrintView crew={crew}/>
-{:else}
-  <CrewData bind:crew={crew} bind:edit={edit}/>
-  <div class="pagebreak"> </div>
-  <div class="grid-container">
-    {#each crew.members as member}
-      <div class="grid-item">
-        <table>
-          <tr class="no-print {edit ? '' : 'hide'}">
-            <td colspan="3">
-              <RemoveButton bind:edit={edit} click={() => crew = crewBuilder.removeMember(member, crew)}/>
-            </td>
-          </tr>
-          <tr>
-            <td rowspan="8">
-              <Photo bind:edit={edit} bind:member={member} bind:crew={crew}/>
-            </td>
-            <th class="r">Name</th>
-            <td class="l"><TextField bind:value={member.name} edit={edit}/></td>
-          </tr>
-          <tr>
-            <th class="r">Faction</th>
-            <td class="l">{crew.faction.name}</td>
-          </tr>
-          <tr>
-            <th class="r">Wounds</th>
-            <td class="l">{member.wounds}</td>
-          </tr>
-          <tr>
-            <th class="r">Combat XP</th>
-            <td class="l">{member.cost}</td>
-          </tr>
-          <tr>
-            <th class="r">Movement</th>
-            <td class="l"><TextField bind:value={member.movement} edit={edit} type="number" change={() => crew = crewBuilder.updateK(crew)}/></td>
-          </tr>
-          <tr>
-            <th class="r">Combat Ability</th>
-            <td class="l"><TextField bind:value={member.combatAbility} edit={edit} type="number" change={() => crew = crewBuilder.updateK(crew)}/></td>
-          </tr>
-          <tr>
-            <th class="r">Armor</th>
-            <td class="l">{member.armor}</td>
-          </tr>
-          <tr>
-            <th class="r">Will</th>
-            <td class="l"><TextField bind:value={member.will} edit={edit} type="number" change={() => crew = crewBuilder.updateK(crew)}/></td>
-          </tr>
-        </table>
-        <Skills bind:crew={crew} bind:member={member} bind:compact={compact} bind:edit={edit}/>
-        <Equipment bind:crew={crew} bind:member={member} bind:compact={compact} bind:edit={edit}/>
-        <WeaponRows bind:crew={crew} bind:member={member} bind:edit={edit}/>
-        <table>
-          <tr class="wide">
-            <th>Notes</th>
-          </tr>
-          <tr class="list wide">
-            <td class="wide fixed">
-              <TextField type="textarea" bind:value={member.notes} edit={edit}/>
-            </td>
-          </tr>
-        </table>
-        <div class="pagebreak"> </div>
-      </div>
-    {/each}
-  </div>
-  {#if edit}
+<CrewData bind:crew={crew} bind:edit={edit}/>
+<div class="pagebreak"> </div>
+<div class="grid-container">
+  {#each crew.members as member}
+    <div class="grid-item">
+      <table>
+        <tr class="no-print {edit ? '' : 'hide'}">
+          <td colspan="3">
+            <RemoveButton bind:edit={edit} click={() => crew = crewBuilder.removeMember(member, crew)}/>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="8">
+            <Photo bind:edit={edit} bind:member={member} bind:crew={crew}/>
+          </td>
+          <th class="r">Name</th>
+          <td class="l"><TextField bind:value={member.name} edit={edit}/></td>
+        </tr>
+        <tr>
+          <th class="r">Faction</th>
+          <td class="l">{crew.faction.name}</td>
+        </tr>
+        <tr>
+          <th class="r">Wounds</th>
+          <td class="l">{member.wounds}</td>
+        </tr>
+        <tr>
+          <th class="r">Combat XP</th>
+          <td class="l">{member.cost}</td>
+        </tr>
+        <tr>
+          <th class="r">Movement</th>
+          <td class="l"><TextField bind:value={member.movement} edit={edit} type="number" change={() => crew = crewBuilder.updateK(crew)}/></td>
+        </tr>
+        <tr>
+          <th class="r">Combat Ability</th>
+          <td class="l"><TextField bind:value={member.combatAbility} edit={edit} type="number" change={() => crew = crewBuilder.updateK(crew)}/></td>
+        </tr>
+        <tr>
+          <th class="r">Armor</th>
+          <td class="l">{member.armor}</td>
+        </tr>
+        <tr>
+          <th class="r">Will</th>
+          <td class="l"><TextField bind:value={member.will} edit={edit} type="number" change={() => crew = crewBuilder.updateK(crew)}/></td>
+        </tr>
+      </table>
+      <Skills bind:crew={crew} bind:member={member} bind:compact={compact} bind:edit={edit}/>
+      <Equipment bind:crew={crew} bind:member={member} bind:compact={compact} bind:edit={edit}/>
+      <WeaponRows bind:crew={crew} bind:member={member} bind:edit={edit}/>
+      <table>
+        <tr class="wide">
+          <th>Notes</th>
+        </tr>
+        <tr class="list wide">
+          <td class="wide fixed">
+            <TextField type="textarea" bind:value={member.notes} edit={edit}/>
+          </td>
+        </tr>
+      </table>
+      <div class="pagebreak"> </div>
+    </div>
+  {/each}
+</div>
+{#if edit}
   <div>
     <label for="recruit">Add recruit</label>
     <select bind:value={selectedRecruit} id="recruit">
@@ -120,16 +119,12 @@
     <button on:click={() => crew = crewBuilder.addRecruit(selectedRecruit, crew)}>Add</button>
   </div>
   <button on:click={saveCrew}>Save</button>
-  {/if}
 {/if}
-{#if !print}
-  <div class="no-print">
-    <button on:click={() => edit = !edit}>{#if edit}Lock{:else}Edit{/if}</button>
-    <button on:click={() => compact = !compact}>{#if compact}Full{:else}Compact{/if}</button>
-    <button on:click={() => print = !print}>{#if print}Regular View{:else}Print View{/if}</button>
-    <button on:click={() => show = false}>Back</button>
-  </div>
-{/if}
+<div class="no-print">
+  <button on:click={() => replace('/crew/' + crew.id + '/' + (edit ? 'view' : 'edit'))}>{#if edit}Lock{:else}Edit{/if}</button>
+  <button on:click={() => compact = !compact}>{#if compact}Full{:else}Compact{/if}</button>
+  <button on:click={() => push('/print/' + crew.id)}>Print View</button>
+</div>
 <style>
   th.r {
     text-align: right;
