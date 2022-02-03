e6410c904af398d42cd448479d18a0b978727b58
Oscar Djupfeldt <oscar@djupfeldt.se>
2022-02-03 21:00:00 +0100

    #5: Redirect to crew page on import

--- src/Import.svelte
+++ src/Import.svelte
@@ -7,12 +7,12 @@
     let importCrew = () => {
         crew = store.importCrew(str)
         if (crew) {
-            push('/print/' + crew.id)
+            push('/crew/' + crew.id + '/edit')
         }
     }
 </script>
 <div>
   <span>Paste either crew json or crew hash (export string).</span>
   <input bind:value={str}>
-  <button on:click={store.importCrew(str)}>Import</button>
+  <button on:click={importCrew(str)}>Import</button>
 </div>
